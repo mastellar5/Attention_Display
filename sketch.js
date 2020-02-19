94b794c0930a4b1b7ef8a5bca6406e71431897a2
mastellar5 <47159390+mastellar5@users.noreply.github.com>
2020-02-19 08:26:59 -0600

    I moved some more face api stuff into libraries and experimented with video playback on another patch

--- sketch.js
+++ sketch.js
@@ -1,27 +1,52 @@
-var capture;
-var w = 640,
-    h = 480;
+// ml5 Face Detection Model
+let faceapi;
+let detections = [];
+
+// Video
+let video;
 
 function setup() {
-    capture = createCapture({
-        audio: false,
-        video: {
-            width: w,
-            height: h
-        }
-    }, function() {
-        console.log('capture ready.')
-    });
-    capture.elt.setAttribute('playsinline', '');
-    createCanvas(w, h);
-    capture.size(w, h);
-    capture.hide();
+  createCanvas(360, 270);
 
-    colorMode(HSB);
-
+  // Creat the video and start face tracking
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  // Only need landmarks for this example
+  const faceOptions = { withLandmarks: true, withExpressions: false, withDescriptors: false };
+  faceapi = ml5.faceApi(video, faceOptions, faceReady);
 }
 
+// Start detecting faces
+function faceReady() {
+  faceapi.detect(gotFaces);
+}
+
+// Got faces
+function gotFaces(error, result) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  detections = result;
+  faceapi.detect(gotFaces);
+  print(result);
+}
+
+// Draw everything
 function draw() {
-    image(capture, 0, 0, w, h);
+  background(0);
+
+  // Just look at the first face and draw all the points
+  if (detections.length > 0) {
+    let points = detections[0].landmarks.positions;
     
+    for (let i = 0; i < points.length; i++) {
+      stroke(161, 95, 251);
+      strokeWeight(4);
+      point(points[i]._x, points[i]._y);
     }
+
+  }
+
+    
+}
